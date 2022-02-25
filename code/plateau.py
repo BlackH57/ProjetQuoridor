8a4d6a6c515bef615cd71deb4869265702119694
BlackH57 <78650826+BlackH57@users.noreply.github.com>
2022-02-25 23:57:42 +0100

    affichagePlateau

--- code/plateau.py
+++ code/plateau.py
@@ -1,6 +1,7 @@
 class Plateau:
     __init__(self, length, player1, player2):
         self.length = length
+        self.computerLength = 2*length
         self.plateau = []
         self.player1 = player1
         self.player2 = player2
@@ -24,3 +25,10 @@ class Plateau:
         
         def placeWall(wall):
             self.placedWall.append(wall)
+
+        def affichagePlateau():
+            for i in range(0,computerLength):
+                res = ""
+                for j in range(0, computerLength):
+                    res = res + str(plateau[i][j])
+                print(res)
