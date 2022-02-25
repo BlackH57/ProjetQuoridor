377d79377db9e270c23da55bc0d0e6fc93f0730c
BlackH57 <78650826+BlackH57@users.noreply.github.com>
2022-02-25 23:54:15 +0100

    initial methods

---
+++ code/plateau.py
@@ -0,0 +1,26 @@
+class Plateau:
+    __init__(self, length, player1, player2):
+        self.length = length
+        self.plateau = []
+        self.player1 = player1
+        self.player2 = player2
+        self.placedWall = []
+        for i in range(0,length):
+            self.plateau.append([0 for i in range(0,length)])
+
+        def setPlayeur(noPlayer, player):
+
+            if(not isinstanceof(noPlayer, int):
+                print("Argument <noPlayer> non valide.")
+                if(not isinstanceof(player, Player):
+                    print("Argument <player> non valide.")
+                return
+            if (noPlayer == 1):
+                self.player1 = player
+            if (noPlayer == 2):
+                self.player2 = player
+            else:
+                print("f{noPplayer} n'est pas un chiffre valide")
+        
+        def placeWall(wall):
+            self.placedWall.append(wall)
