85ca722b947f7ad73bc33dd754f1657fc1d11ce5
BlackH57 <78650826+BlackH57@users.noreply.github.com>
2022-02-28 09:58:36 +0100

    Update game.py

---
+++ code/game.py
@@ -0,0 +1,14 @@
+from codeClass import plateau
+
+class Game:
+
+	def __init__(self, plateau):
+		self.plateau = plateau
+		self.nbTour = 0
+
+	def jouer(self):
+		player.jouer()
+
+	def actualise(self):
+		self.nbTour+=1
+		self.jouer()
